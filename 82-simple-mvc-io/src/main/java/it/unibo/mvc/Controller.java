a75a97fbae83897a2b168ff496c81abac1c395bd
Enrico Marchionni <enrico.marchionni@studio.unibo.it>
2022-11-18 18:50:03 +0100

    Exercise 82-simple-mvc-io done

--- 82-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 82-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -10,38 +10,57 @@ import java.nio.charset.StandardCharsets;
  * Application controller. Performs the I/O.
  */
 public class Controller {
-    private File file = new File(System.getProperty("user.home") + System.getProperty("file.separator") + "output.txt");
 
-    public void setFile(final File file) {
+    private static String defaultFileName = "output.txt";
+    private File file = new File(System.getProperty("user.home") + System.getProperty("file.separator") + defaultFileName);
+
+    /**
+     * This method changes the currently selected {@link File}.
+     * If not setted the {@link File} is the one specified
+     * by the field {@link defaultFileName}.
+     * @param file
+     */
+    public final void setFile(final File file) {
         this.file = file;
     }
 
-    public File getFile() {
+    /**
+     * @return this value could be the one 
+     * setted in {@link setFile}or the
+     * default one.
+     */
+    public final File getFile() {
         return this.file;
     }
 
-    public String getFilePath() {
+    /**
+     * @return the path associated to the currently selected file.
+     * N.B. path could not exist.
+     */
+    public final String getFilePath() {
         return this.file.getAbsolutePath();
     }
 
-    public boolean writeContent(final String buffer) {
-        boolean result = true;
-        if (!file.exists()) {
-            try {
-                file.createNewFile();
-            } catch (IOException e) {
-                System.out.println("Error in creating file"); // NOPMD: allowed as this is just an exercise
-                e.printStackTrace(); // NOPMD: allowed as this is just an exercise
-                result = false;
-            }
+    /**
+     * This method write a {@link String} in the currently selected file.
+     * @param buffer the buffer to be written.
+     * @return true indicates success, false error in writing.
+     */
+    public final boolean writeContent(final String buffer) {
+        boolean result = false;
+        try {
+            result = file.createNewFile();
+        } catch (IOException e) {
+            System.out.println("Error in creating file"); // NOPMD: allowed as this is just an exercise
+            e.printStackTrace(); // NOPMD: allowed as this is just an exercise
         }
 
         try (BufferedWriter r = new BufferedWriter(new FileWriter(file, StandardCharsets.UTF_8))) {
             r.write(buffer);
+            result = true;
         } catch (IOException e) {
             System.out.println("Error in writing file"); // NOPMD: allowed as this is just an exercise
             e.printStackTrace(); // NOPMD: allowed as this is just an exercise
-            result = false;
         }
 
         return result;
